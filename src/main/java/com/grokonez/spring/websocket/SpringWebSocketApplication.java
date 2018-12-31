2fcd634dcce73323f41bee2b93f90265821c6539
omprakash <omprakash.jaiswal@stixis.com>
2018-12-31 13:35:35 +0530

    base

---
+++ src/main/java/com/grokonez/spring/websocket/SpringWebSocketApplication.java
@@ -0,0 +1,12 @@
+package com.grokonez.spring.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketApplication {
+	//https://grokonez.com/spring-framework/spring-boot/angular-6-websocket-example-with-spring-boot-websocket-server-sockjs-stomp
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSocketApplication.class, args);
+	}
+}
